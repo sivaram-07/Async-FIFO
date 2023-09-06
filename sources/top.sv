ee2d1ceccd8d6d57cdf590dd2c14f3361e1a8c9b
Sivaram Durai <114935240+sivaram-07@users.noreply.github.com>
2023-09-06 17:52:55 +0530

    Update top.sv

--- sources/top.sv
+++ sources/top.sv
@@ -12,18 +12,15 @@ wire [$clog2(DEPTH)-1:0] waddr,raddr;
 wire [$clog2(DEPTH):0] wptr,rptr,wsync_ptr2,rsync_ptr2;
 
 
-sync_r2w #(DEPTH) sync_r2w(.w_clk(w_clk), .rst_n(rst_n),.wsync_ptr2(wsync_ptr2), .rptr(rptr));
+sync_r2w #(DEPTH) sync_r2w(.*);
 
-sync_w2r #(DEPTH) sync_w2r(.r_clk(r_clk), .rst_n(rst_n),.rsync_ptr2(rsync_ptr2), .wptr(wptr));
+sync_w2r #(DEPTH) sync_w2r(.*);
 
-fifo_mem #(WIDTH,DEPTH) fifomem
-(.w_clk(w_clk),.rdata(rdata),.wdata(wdata),.waddr(waddr), .raddr(raddr),.full(full),.empty(empty),.wr_rq(wr_rq),.rd_rq(rd_rq));
+fifo_mem #(WIDTH,DEPTH) fifomem(.*);
 
-full #(WIDTH,DEPTH) full_1
-(.w_clk(w_clk),.rst_n(rst_n),.full(full), .waddr(waddr),.wptr(wptr), .wsync_ptr2(wsync_ptr2),.wr_rq(wr_rq));
+full #(WIDTH,DEPTH) full_1(.*);
 
-empty  #(WIDTH,DEPTH) empty_1
-(.r_clk(r_clk),.rst_n(rst_n),.empty(empty), .raddr(raddr),.rptr(rptr), .rsync_ptr2(rsync_ptr2),.rd_rq(rd_rq));
+empty  #(WIDTH,DEPTH) empty_1(.*);
 
 
 
